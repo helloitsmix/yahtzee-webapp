bedd190b6ea7789fcaa2ccb496ad817cec8bfb11
vezzi98 <vezzarolorenzo@gmail.com>
2019-12-11 11:57:56 +0100

    aggiunta animazione col completa, calcolo totale, TO DO: check se col piena, new game, schermata new game, nome, calcolo tot tot

--- js/main.js
+++ js/main.js
@@ -6,17 +6,36 @@ let nomiColonne = [
     'annunciati'
 ];
 
+let Setup = () => {
+    
+    // $('.numeri input, .combo input').val(''); TUTTE LE CASELLE TORNANO VUOTE
+    $('.bonus input').val(0);
+    
+    nomiColonne.forEach(nomeColonna => { 
+        $('.numeri .' + nomeColonna + ' input').on('input propertychange', () => {
+            CalcoloBonus();
+        });
+    });
+
+    $("input[name='salire-input']").on('input propertychange', () => {
+        UpDownUnlock();
+    });
+    $("input[name='scendere-input']").on('input propertychange', () => {
+        UpDownUnlock();
+    });
+
+};
 
 let CalcoloBonus = () => {
     
     nomiColonne.forEach(nomeColonna => { 
 
-        let totalPoints = 0;
-        $("input[name='" + nomeColonna + "-input']").each( (index, item) => {
+        let bonusPoints = 0;
+        $('.numeri .' + nomeColonna + ' input').each( (index, item) => {
             if ( !isNaN( parseInt( $(item).val() )))
-                totalPoints += parseInt( $(item).val() );
+                bonusPoints += parseInt( $(item).val() );
         });
-        $("input[name='" + nomeColonna + "-bonus']").val(totalPoints >= 60 ? totalPoints * 2 : totalPoints);
+        $('.bonus .' + nomeColonna + '-bonus input').val(bonusPoints >= 60 ? bonusPoints * 2 : bonusPoints);
 
     });
 
@@ -24,11 +43,21 @@ let CalcoloBonus = () => {
 
 let UpDownUnlock = () => {
 
-    $("input[name='scendere-input']").each( (index, item) => {
+    $('.scendere input').each( (index, item) => {
 
-        if ( $(item).prop('disabled') && parseInt( $("input[name='scendere-input']").eq(index-1).val() ) >= 0 && !isNaN(parseInt( $("input[name='scendere-input']").eq(index-1).val() )) )
+        if ( $(item).prop('disabled') && parseInt( $('.scendere input').eq(index-1).val() ) >= 0 && !isNaN(parseInt( $('.scendere input').eq(index-1).val() )) )
         {
-            $("input[name='scendere-input']").eq(index).prop('disabled', false);
+            $('.scendere input').eq(index).prop('disabled', false);
+            return false;
+        }
+
+    });
+
+    $('.salire input').each( (index, item) => {
+        
+        if ( !$(item).prop('disabled') && parseInt( $('.salire input').eq(index).val() ) >= 0 && !isNaN(parseInt( $('.salire input').eq(index).val() )) )
+        {
+            $('.salire input').eq(index-1).prop('disabled', false);
             return false;
         }
 
@@ -36,9 +65,52 @@ let UpDownUnlock = () => {
 
 };
 
-nomiColonne.forEach(nomeColonna => { 
-    $("input[name='" + nomeColonna + "-input']").on('input propertychange', () => {
-        CalcoloBonus();
-        UpDownUnlock();
+let Animate = (nomeColonna, classe) => {
+
+    $('.' + nomeColonna + ' input').each( (index, item) => {
+
+        let delay = index * 100;
+        $(item).delay(delay).addClass(classe,900).delay( 100 ).removeClass(classe,900);
+        
     });
-});
+
+    setTimeout(function() {
+        CalcoloTotale(nomeColonna, classe);
+    }, 2700);
+
+};
+
+let CalcoloTotale = (nomeColonna, classe) => {
+    
+    let totalPoints = parseInt( $('.bonus .' + nomeColonna + '-bonus input').val() );
+    
+    $('.combo .' + nomeColonna +' input').each( (index, item) => {
+        if ( !isNaN( parseInt( $(item).val() )))
+                totalPoints += parseInt( $(item).val() );
+    });
+
+    nomeColonna === 'secchi' ? totalPoints *= 2 : nomeColonna === 'annunciati' ? totalPoints *= 3 : '';
+    
+    $('.totale .' + nomeColonna + '-totale input').addClass(classe,1000);
+    
+    $({percentage: 0}).stop(true).animate({percentage: totalPoints}, {
+        duration : 2700,
+        easing: "easeOutExpo",
+        step: function () {
+            var percentageVal = Math.round(this.percentage);
+            $('.totale .' + nomeColonna + '-totale input').val(percentageVal);
+        }
+    }).promise().done(function () {
+        // hard set the value after animation is done to be sure the value is correct
+        $('.totale .' + nomeColonna + '-totale input').val(totalPoints);
+    });
+
+};
+
+Setup();
+
+
+// TODO
+// function ControlloColonnaCompletata
+// if (colonna completata) {
+// Animate('liberi', 'highlight-yellow'); }
