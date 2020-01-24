822d237c9eb2965a2b356d699351064ca8b09543
helloitsmix <lorenzovezzaro@yahoo.it>
2020-01-24 17:58:42 +0100

    Aggiunta home

--- js/main.js
+++ js/main.js
@@ -19,7 +19,23 @@ let arrayDaSalvare = new Array(60);
 
 // TOGLIERE IL RELOAD PAGINA CON SWIPE UP?
 
-$("#hamburger-settings").click( function() {
+$('#button-home').click( e => {
+    e.preventDefault();
+    if (localStorage.getItem('username') === null)
+    {
+        if ($('#input-home').val() !== "")
+        {
+            localStorage.setItem('username', $('#input-home').val())
+            $('.homepage').addClass('swiped');
+        }
+    }
+    else
+    {
+        $('.homepage').addClass('swiped');
+    }
+});
+
+$('#hamburger-settings').click( function() {
     $('.icon').toggleClass('close');
     $('.settings-page').toggleClass('open');
     $('.tabellone').toggleClass('blurred');
@@ -107,6 +123,51 @@ let NuovaPartita = () => {
     localStorage.removeItem('data');
 };
 
+let CreaMessaggioRandom = () => {
+    let messaggio = "";
+    let numeroRandom = Math.floor(Math.random() * 10); // da 0 a 9;
+    let username = localStorage.getItem('username');
+
+    switch (numeroRandom)
+    {
+        case 0:
+            messaggio = "Ciao " + username + ", giochiamo?";
+            break;
+        case 1:
+            messaggio = "Bentornato " + username;
+            break;
+        case 2:
+            messaggio = "Hey " + username + " ;)";
+            break;
+        case 3:
+            messaggio = "Ancora qui?";
+            break;
+        case 4:
+            messaggio = "Ti senti fortunato oggi?";
+            break;
+        case 5:
+            messaggio = "Finalmente sei tornato ^.^";
+            break;
+        case 6:
+            messaggio = username + ", bel nome";
+            break;
+        case 7:
+            messaggio = username + ", che brutto nome";
+            break;
+        case 8:
+            messaggio = "Ti sei seriamente chiamato " + username + "?";
+            break;
+        case 9:
+            messaggio = "Oggi si fa il record!";
+            break;
+        default:
+            messaggio = "Ciao!";
+            break;
+    }
+
+    return messaggio;
+};
+
 let Setup = () => {
     
     $('.bonus input').val(0);
@@ -146,6 +207,20 @@ let Setup = () => {
 };
 
 let CheckIniziale = () => {
+
+    if (localStorage.getItem('username') === null)
+    {
+        // fa vedere l'input
+        $('#input-home').removeClass('hide');
+        $('.message').addClass('hide');
+    }
+    else
+    {
+        // stampa messaggi random
+        $('#input-home').addClass('hide');
+        $('.message').removeClass('hide');
+        $('.message').text(CreaMessaggioRandom());
+    }
 
     if (localStorage.getItem('table-size') == 'small') {
         SetGrigliaSmall();
